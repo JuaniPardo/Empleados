2a0fd9c5080b3dc97c95c0c393bd27b9af3a6341
Juani Pardo <118234203+JuaniPardo@users.noreply.github.com>
2023-04-17 22:14:16 -0300

    Commit Inicial

---
+++ empleados/EmpleadoTest.java
@@ -0,0 +1,45 @@
+package empleados;
+
+public class EmpleadoTest {
+    public static void main(String[] args) {
+        Empleado[] empleados = new Empleado[3];
+
+        //HAGO UN CICLO PARA CREAR "i" OBJETOS
+        for (int i = 0; i < 3; i++) {
+            empleados[i] = new Empleado();
+        }
+        //SETEO ID
+        empleados[0].setId(918);
+        empleados[1].setId(861);
+        empleados[2].setId(737);
+
+        //SETEO NOMBRES
+        empleados[0].setNombre("Martina");
+        empleados[1].setNombre("Catalina");
+        empleados[2].setNombre("Amelia");
+
+        //SETEO APELLIDOS
+        empleados[0].setApellido("Ríos");
+        empleados[1].setApellido("Medina");
+        empleados[2].setApellido("González");
+
+        //SETEO SALARIOS
+        empleados[0].setSalario(483.33);
+        empleados[1].setSalario(577.73);
+        empleados[2].setSalario(457.43);
+
+        //HAGO UN CICLO PARA MOSTRAR LOS DATOS
+        for (int i = 0; i < 3; i++) {
+            System.out.println("EMPLEADO " + (i+1) + "\n" + "ID: " + empleados[i].getId()+ "\nNombre: " + empleados[i].getNombre() + "\nApellido: " + empleados[i].getApellido() + "\nSalario: " + empleados[i].getSalario() + "\n");
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/empleados/Empleado.java
@@ -0,0 +1,35 @@
+package empleados;
+
+public class Empleado {
+
+    //##### DECLARACION VARIABLES INSTANCIA ######
+    private int id; //PRIMITIVO
+    private String nombre; //REFERENCIA
+    private String apellido; //REFERENCIA
+    private double salario; //PRIMITIVO
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
